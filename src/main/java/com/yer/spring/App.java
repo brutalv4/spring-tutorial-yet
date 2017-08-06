d868d15f2e9882cf0c69a02a19fbfb0d9d3a06a1
Vladimir Bredikhin <brutal.v4@gmail.com>
2017-08-06 22:08:49 +0300

    up2date: time 4 Spring

---
+++ src/main/java/com/yer/spring/App.java
@@ -0,0 +1,27 @@
+package com.yer.spring;
+
+import com.yer.spring.domain.Client;
+import com.yer.spring.domain.EventLogger;
+
+public class App {
+    private Client client;
+    private EventLogger eventLogger;
+
+    public App(Client client, EventLogger eventLogger) {
+        this.client = client;
+        this.eventLogger = eventLogger;
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        app.logEvent("Some event for user 1");
+    }
+
+    public void logEvent(String msg) {
+        String message = msg.replaceAll(client.getId(), client.getFullName());
+        eventLogger.logEvent(message);
+    }
+
+    public App() {
+    }
+}
